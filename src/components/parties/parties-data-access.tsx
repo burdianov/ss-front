8b90e4439b11062ecfd7cab0086f9e3e42286623
Lilian Burdianov <lilian.burdianov@gmail.com>
2024-11-29 15:08:47 +0400

    work in progress

--- src/components/parties/parties-data-access.tsx
+++ src/components/parties/parties-data-access.tsx
@@ -1,6 +1,6 @@
 import { getSsProgram, getSsProgramId } from "@project/anchor";
 import { useConnection } from "@solana/wallet-adapter-react";
-import { Cluster, Keypair, PublicKey } from "@solana/web3.js";
+import { Cluster, PublicKey } from "@solana/web3.js";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 import { useMemo } from "react";
@@ -17,22 +17,19 @@ export function useSsProgram() {
   const programId = useMemo(() => getSsProgramId(cluster.network as Cluster), [cluster]);
   const program = getSsProgram(provider);
 
-  console.log("programId", programId);
-  console.log("cluster", cluster);
-
   const partiesAccounts = useQuery({
-    queryKey: ["parties-accounts", "all", { cluster }],
+    queryKey: ["parties-accounts", "all"],
     queryFn: () => program.account.parties.all(),
   });
 
   const getProgramAccount = useQuery({
-    queryKey: ["get-program-account", { cluster }],
+    queryKey: ["get-program-account"],
     queryFn: () => connection.getParsedAccountInfo(programId),
   });
 
   const initialize = useMutation({
-    mutationKey: ["parties", "initialize", { cluster }],
-    mutationFn: (keypair: Keypair) => program.methods.initialize().accounts({ organizer: keypair.publicKey }).rpc(),
+    mutationKey: ["parties", "initialize"],
+    mutationFn: (publicKey: PublicKey) => program.methods.initialize().accounts({ organizer: publicKey }).rpc(),
     onSuccess: (signature) => {
       transactionToast(signature);
       return partiesAccounts.refetch();
@@ -40,5 +37,5 @@ export function useSsProgram() {
     onError: () => toast.error("Failed to initialize parties"),
   });
 
-  return { initialize, getProgramAccount, partiesAccounts };
+  return { initialize, getProgramAccount, partiesAccounts, programId };
 }
--- src/components/parties/parties-feature.tsx
+++ src/components/parties/parties-feature.tsx
@@ -1,7 +1,23 @@
+import { useWallet } from "@solana/wallet-adapter-react";
+import { useSsProgram } from "./parties-data-access.tsx";
+
 export default function PartiesFeature() {
-  return (
-    <>
-      <h2>Parties component</h2>
-    </>
+  const { publicKey } = useWallet();
+  const { programId, initialize } = useSsProgram();
+
+  return publicKey ? (
+    <div className="text-center">
+      <h2>Public key: {publicKey.toString()}</h2>
+      <h2>ProgramId: {programId.toString()}</h2>
+      <button
+        className="btn btn-xs lg:btn-md btn-primary"
+        onClick={() => initialize.mutateAsync(publicKey)}
+        disabled={initialize.isPending}
+      >
+        Create {initialize.isPending && "..."}
+      </button>
+    </div>
+  ) : (
+    <h2>You need to connect your wallet</h2>
   );
 }
